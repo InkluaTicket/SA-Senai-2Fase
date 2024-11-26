a60514de7fe3fe51a94664ea7d6fec41df429478
DarlanHildebrando <darlan_m_silva@estudante.sesisenai.org.br>
2024-11-26 17:07:21 -0300

    pqp

--- SA InkluaTicket/src/components/SearchBar.jsx
+++ SA InkluaTicket/src/components/SearchBar.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import '../styles/SearchBar.css'
 
 
-<<<<<<< HEAD
 function SearchBar() {
 
   const handlekey = (e) => {
@@ -14,20 +13,12 @@ function SearchBar() {
   return (
     
       <div className="search-bar">
-=======
-
-function SearchBar() {
-
-  return (
-      <div className='barra-pesquisa'>
->>>>>>> 55b652dcf04a2d9a2e78198d542134e30daecd32
       <input
         type="search"
         title='Barra de pesquisa'
         aria-label='Barra de pesquisa'
         className="search-input"
         placeholder="Pesquisar..."
-<<<<<<< HEAD
       />
       <button className="search-button" onClick={handlekey}><img className='IconSearch' src="./img/Vector.png" alt="Pesquisar" /></button>
     </div>
@@ -36,15 +27,3 @@ function SearchBar() {
 }
 
 export default SearchBar
-=======
-        tabindex="1"
-      />
-      
-      <button className="search-button"><img src="./img/lupa.png" alt="Pesquisar" className='lupa' /></button>
-    </div>
-
-  )
-}
-
-export default SearchBar;
->>>>>>> 55b652dcf04a2d9a2e78198d542134e30daecd32
