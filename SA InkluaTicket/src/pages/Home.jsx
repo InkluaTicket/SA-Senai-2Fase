c5d60c1d23e0c8c718fbeb0948c76f444cdad72a
DarlanHildebrando <darlan_m_silva@estudante.sesisenai.org.br>
2024-11-26 16:46:59 -0300

    tela cad

--- SA InkluaTicket/src/pages/Home.jsx
+++ SA InkluaTicket/src/pages/Home.jsx
@@ -1,21 +1,11 @@
 import React from 'react'
-<<<<<<< HEAD
 import NavBarForHome from '../components/NavbarForHome'
 import '../styles/Home.css'
-=======
-import Logo from '../components/Logo'
-import NavbarHome from '../components/NavbarHome'
-import "../styles/Home.css"
->>>>>>> 55b652dcf04a2d9a2e78198d542134e30daecd32
 
 function Home() {
   return (
     <div>
-<<<<<<< HEAD
       <NavBarForHome/>
-=======
-      <NavbarHome/>
->>>>>>> 55b652dcf04a2d9a2e78198d542134e30daecd32
       
     </div>
   )
--- SA InkluaTicket/src/router/Routes.jsx
+++ SA InkluaTicket/src/router/Routes.jsx
@@ -2,29 +2,19 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 
 import Home from "../pages/Home";
-<<<<<<< HEAD
 import EscolhaLogin from "../pages/EscolhaLogin";
 import EscolhaCadastro from "../pages/EscolhaCadastro";
 import TelaCadastro from "../pages/TelaCadastro"
 
 
-=======
-import SobreNos from "../pages/SobreNos";
-import CriarEvento from "../pages/CriarEvento";
->>>>>>> 55b652dcf04a2d9a2e78198d542134e30daecd32
 
 const Routes = createBrowserRouter([
 
 {path: '/', element: <Home/>},
-<<<<<<< HEAD
 {path: '/EscolhaLogin', element: <EscolhaLogin/>},
 {path: '/EscolhaCadastro', element: <EscolhaCadastro/>},
 {path: 'CadastroUser', element: <TelaCadastro/>}
 
-=======
-{path: '/SobreNos', element: <SobreNos/>},
-{path: '/CriarEvento', element: <CriarEvento/>},
->>>>>>> 55b652dcf04a2d9a2e78198d542134e30daecd32
 
 ])
 
