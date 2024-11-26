9e04e191baafe20d4b20e0748c89dee6ad5c1829
L7noxy <gabrielcastanhel2@gmail.com>
2024-11-26 16:51:12 -0300

    Atualização

--- SA InkluaTicket/src/components/NavbarHome.jsx
+++ SA InkluaTicket/src/components/NavbarHome.jsx
@@ -9,29 +9,30 @@ function NavbarHome() {
             <div className="EndElementsHome">
                 <img
                     className="LogoFundoAzulHome"
-                    src="./img/LogoInkluaAzul.png"
+                    src="./img/logo.png"
                     alt="Logo Inklua"
                 />
                 <SearchBar />
             </div>
 
             <nav className="InfosHome">
-                <Link
-                    to="CriarEvento"
-                    role="button"
-                    tabIndex={0}
-                    className="InfosNavHome"
-                >
+                
+                <Link to="CriarEvento" role="button" tabIndex={0} className="InfosNavHome">
                     Criar seu evento
                 </Link>
+
                 <div className="separadorHome"></div>
+
                 <Link to="/login" tabIndex={0} className="InfosNavHome">
                     Acessar minha conta
                 </Link>
+
                 <div className="separadorHome"></div>
+
                 <Link to="/signup" tabIndex={0} className="InfosNavHome">
                     Cadastrar-se
                 </Link>
+
             </nav>
         </header>
     );
