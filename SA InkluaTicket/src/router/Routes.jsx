523ae91179e972ad9ca5b2e34fc7efd0cc646ae9
DarlanHildebrando <darlan_m_silva@estudante.sesisenai.org.br>
2024-12-01 21:17:01 -0300

    atualização geral 2

--- SA InkluaTicket/src/router/Routes.jsx
+++ SA InkluaTicket/src/router/Routes.jsx
@@ -6,7 +6,6 @@ import TelaCadastro from "../pages/TelaCadastro";
 import CriarEvento from "../pages/CriarEvento";
 import TelaUsuarioOuEmpresa from "../pages/TelaUsuarioOuEmpresa";
 import TelaLogin from "../pages/TelaLogin";
-import TelaCadastroEmpresa from "../pages/TelaCadastroEmpresa";
 import TelaUsuarioOuEmpresaLogin from "../pages/TelaUsuarioOuEmpresaLogin";
 import Gerenciamento from "../pages/Gerenciamento";
 
@@ -25,7 +24,6 @@ const Routes = createBrowserRouter([
   {path: '/', element: <PageWrapper element={ <Home />} screenName='Tela de início'/> },
   {path: '/TelaLogin', element: <PageWrapper element={ <TelaLogin />} screenName='Tela de login para usuários'/>},
   { path: '/CadastroUser', element: <PageWrapper element={ <TelaCadastro />} screenName='Tela de cadastro para usuários' /> },
-  { path: '/CadastroEmpresa', element: <PageWrapper element={ <TelaCadastroEmpresa />} screenName='Tela de cadastro para empresas' /> },
   { path: '/CriarEvento', element: <PageWrapper element={ <CriarEvento />} screenName='Tela de criação de eventos'/> },
   { path: '/EscolhaCadastro', element: <PageWrapper element={ <TelaUsuarioOuEmpresa />} screenName='Tela de escolha, forma de cadastro' /> },
   { path: '/EscolhaLogin', element: <PageWrapper element={ <TelaUsuarioOuEmpresaLogin />} screenName='Tela de escolha, forma de login' /> },
