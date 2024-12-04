fa6b6b6b29d9d9232cd81c7111b1b3122aa8cd3a
JOSÉ VINÍCIUS VICTOR DAUZAKER <jose_dauzaker@estudante.sesisenai.org.br>
2024-12-04 16:21:57 -0300

    tela de perfil de empresa

--- SA InkluaTicket/src/components/NavbarPerfilEmpresa.jsx
+++ SA InkluaTicket/src/components/NavbarPerfilEmpresa.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import '../styles/NavbarPerfilEmpresa.css';
 
 function NavbarPerfilEmpresa() {
   return (
     <div>
+      <div className='navbar-containerEmpresa'>
       
+      <div className='div-navbarEmp'>
+
+      <img className='voltarNavEmp' src="./img/arrow_back (2).png" alt="" />
+      <h1 className='perfilEmpresa'>Perfil Empresarial</h1>
+
+      </div>
+
+      <div><img className='logo' src="./img/InKlua.png" alt="" /></div>
+
+    </div>
     </div>
   )
 }
--- SA InkluaTicket/src/pages/PerfilEmpresa.jsx
+++ SA InkluaTicket/src/pages/PerfilEmpresa.jsx
@@ -1,20 +1,54 @@
 import React from 'react'
+import '../styles/PerfilEmpresa.css';
+import NavbarPerfilEmpresa from '../components/NavbarPerfilEmpresa';
 
 function PerfilEmpresa() {
   return (
     <div>
+      <NavbarPerfilEmpresa/>
+<div className='Corpo-containerEmpresa'>
 
-        <div className='container-perfilEmpresa'>
+<div className='div1Emp'>
 
-        <div className='div-navbarEmp'>
+  <img className='iconEmp' src="./img/fotoUser.png" />
+  <img className='verificadoEmp' src="./img/verif-empresa.png"/>
+  <p className='nomeEmp'>InKluaTicket</p>
 
-        <img className='voltarNavEmp' src="./img/arrow_back (2).png" alt="" />
-        <h1 className='perfilEmpresa'>Perfil de Empresa</h1>
-        
-        </div>
-        
-        <div><img className='logoNaveEmp' src="./img/InKlua.png" alt="" /></div>
-        </div>
+
+  <label className='labelEmp'>Email para contato:</label>
+  <input className='inptsEmp' type="text" />
+
+  <label className='labelEmp'>Telefone para contato:</label>
+  <input className='inptsEmp' type="text" />
+
+  <label className='labelEmp'>Endereço fiscal:</label>
+  <input className='inptsEmp' type="text" />
+
+</div>
+
+
+<div className="divLinhaEmp"></div>
+
+
+<div className='div2Emp'>
+  
+  <h1 className='Frase1Emp'>Proteja as informações da sua empresa!</h1>
+
+  <p className='Texto1Emp'>Garanta que apenas pessoas autorizadas tenham acesso a este perfil. 
+  Suas informações são tratadas com segurança e utilizadas para otimizar a gestão de sua empresa.</p>
+
+    <h1 className='Frase2Emp'>Mantenha os dados atualizados!</h1>
+  
+  <p className='Texto2Emp'>Informações precisas garantem uma comunicação eficiente e uma experiência mais personalizada para sua empresa e seus clientes.</p>
+
+  <button className='gerenciarEmp'>Gerenciar Perfil</button>
+  <img className='iconGerenciarEmp' src="./img/gerenciamentoUser.png"/>
+  
+  
+</div>
+
+
+</div>
       
     </div>
   )
