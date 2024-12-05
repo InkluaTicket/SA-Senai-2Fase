4041487e8b0a28a9fd54ab8aee1dd31a37aebc5c
GABRIEL DA CUNHA CASTANHEL <gabriel_castanhel@estudante.sesisenai.org.br>
2024-12-05 19:43:00 -0300

    Fazendo a tela de Eventos

--- SA InkluaTicket/src/pages/Shows.jsx
+++ SA InkluaTicket/src/pages/Shows.jsx
@@ -1,32 +1,28 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar.jsx";
 import "../styles/Shows.css";
 
 function Shows() {
   return (
-    <>
+    <div>
       <Navbar />
-      <div class="evento-container">
-        <header class="evento-header">
-          <h1>Numanice Rio de Janeiro</h1>
-          <p>Data Extra - Riocentro - 08 de Dezembro, 2024</p>
-        </header>
-        <section class="evento-banner">
-          <img src="" alt="Imagem do evento" />
-        </section>
-        <section class="evento-descricao">
-          <h2>Descrição do Evento</h2>
-          <p>
-            Prepare-se para uma experiência inesquecível com Numanice. Música,
-            alegria e muita energia no Riocentro!
-          </p>
-          <button class="btn-comprar">Compre seu ingresso</button>
-        </section>
-        <footer class="evento-rodape">
-          <p>© 2024 Numanice | Todos os direitos reservados.</p>
-        </footer>
+      <div className="img-container">
+        <img 
+        src="./img/ClimaCarbono.png" 
+        className="img-frente" 
+        />
       </div>
-    </>
+
+      <div>
+        <img
+          src="./img/ClimaCarbono2.png"
+          className="img-fundo"
+          alt="Imagem do evento"
+        />
+      </div>
+
+      <div className="content"></div>
+    </div>
   );
 }
 
