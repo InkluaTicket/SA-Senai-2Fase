88ac7562967e9cb2a2736db22d6fcc271d1734ec
josehvinii <jvictordauzaker@gmail.com>
2024-12-08 17:37:12 -0300

    tela de gerenciamento de empresa 1.0

---
+++ SA InkluaTicket/src/components/NavbarGerenEmpre.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../styles/NavbarGerenEmpre.css';
+
+function NavbarGerenEmpre() {
+  return (
+    <div>
+
+      <div className='navbar-containerGE'>
+      
+      <div className='teste-gerenEmpr'>
+
+      <img className='voltarGE' src="./img/arrow_back (2).png" alt="" />
+      <h1 className='gerenciamentoDaContaEmpresa'>Gerenciamento da conta de empresa</h1>
+
+      </div>
+
+      <div><img className='logoGE' src="./img/InKlua.png" alt="" /></div>
+
+    </div>
+
+    </div>
+  )
+}
+
+export default NavbarGerenEmpre
---
+++ SA InkluaTicket/src/pages/GerenciamentoEmpresa.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/GerenciamentoEmpresa.css';
+
+function GerenciamentoEmpresa() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default GerenciamentoEmpresa
